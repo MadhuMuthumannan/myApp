9bae4198e64a007800d467f3442ab3634e914c9d
Madhu <madhu.muthumannan@gmail.com>
2017-02-22 13:26:30 +0530

    first commit

---
+++ public/js/appRoutes.js
@@ -0,0 +1,28 @@
+// public/js/appRoutes.js
+    angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+        // home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainController'
+        })
+
+        // nerds page that will use the NerdController
+        .when('/nerds', {
+            templateUrl: 'views/nerd.html',
+            controller: 'NerdController'
+        })
+
+        // nerds page that will use the NerdController
+        .when('/blog', {
+            templateUrl: 'views/blog.html',
+            controller: 'BlogController'
+        })
+        
+
+
+    $locationProvider.html5Mode(true);
+
+}]);
